10cae7e536dfb8b93384f78668329363393e39b8
Julianorm <99045005+Julianorm@users.noreply.github.com>
2026-06-14 18:02:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import{useState,useCallback,useMemo,useEffect}from 'react'
+import{useState,useCallback,useMemo,useEffect}from  'react'
 import*as XLSX from 'xlsx'
 import{supabase}from './lib/supabase'
 import{useAuth}from './lib/useAuth'
